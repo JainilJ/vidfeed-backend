3a40e630f6a87449bd0807bb464d5be16e93d9ef
jainilj <202301032@dau.ac.in>
2026-01-26 20:25:37 +0530

    Routes and controllers setup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
+
+// middlewares
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
@@ -11,5 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// router import
+import userRoutes from "./routes/user.routes.js";
+
+// routes
+app.use("/api/v1/users", userRoutes);
 
 export {app};
