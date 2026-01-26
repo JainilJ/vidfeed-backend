95bbe5bfbb1c50cec58ba6b94dc8885bc831bc75
jainilj <202301032@dau.ac.in>
2026-01-26 17:30:02 +0530

    Add user and video models

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";   
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000, ()=>{
+        console.log(`\n SERVER STARTED AT PORT: ${process.env.PORT} \n`);
+    })
+})
+.catch((err)=>console.error("Error in initial DB connection:", err));
