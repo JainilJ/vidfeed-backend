14b7aac51ccd646cb8ac835ce5d3f2b22d4a4fa7
jainilj <202301032@dau.ac.in>
2026-02-04 11:34:56 +0530

    Add user channel profile and watch history retrieval features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,5 +22,7 @@ router.route("/me").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/channel/:username").get( getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
